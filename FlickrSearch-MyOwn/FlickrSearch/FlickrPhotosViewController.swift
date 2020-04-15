30dc68ba804eb9535571cc0a249559e6e57d371d
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-15 10:03:47 +0900

    TextFieldから検索デモまで実装

--- FlickrSearch-MyOwn/FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch-MyOwn/FlickrSearch/FlickrPhotosViewController.swift
@@ -7,6 +7,47 @@ final class FlickrPhotosViewController: UICollectionViewController {
                                              left: 20.0,
                                              bottom: 50.0,
                                              right: 20.0)
+  private var searches: [FlickrSearchResults] = []  // 各検索結果を保存する
+  private let flickr = Flickr()  // Flickr検索用
   
 }
 
+
+// MARK:- Private
+private extension FlickrPhotosViewController {
+  // indexを指定して画像オブジェクトを取得するための関数
+  func photo(for indexPath: IndexPath) -> FlickrPhoto {
+    return searches[indexPath.section].searchResults[indexPath.row]
+  }
+}
+
+
+// MARK: - Text Field Delegate
+extension FlickrPhotosViewController : UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // 1
+    let activityIndicator = UIActivityIndicatorView(style: .gray)
+    textField.addSubview(activityIndicator)
+    activityIndicator.frame = textField.bounds
+    activityIndicator.startAnimating()
+    
+    flickr.searchFlickr(for: textField.text!) { searchResults in
+      // 検索が完了した後の処理
+      activityIndicator.removeFromSuperview()
+      
+      switch searchResults {
+      case .error(let error) :
+        print("Error Searching: \(error)")
+      case .results(let results):
+        print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+        self.searches.insert(results, at: 0)
+        // 4
+        self.collectionView?.reloadData()
+      }
+    }
+    
+    textField.text = nil
+    textField.resignFirstResponder()
+    return true
+  }
+}
